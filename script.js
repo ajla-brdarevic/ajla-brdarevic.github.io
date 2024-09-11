af4722876f40abec4849ed8fe32b13a6387bbb23
Ajla Brdarević <93090001+ajla-brdarevic@users.noreply.github.com>
2024-09-11 18:56:19 +0200

    nesto sam pokvarila haman

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    //tajmer za splash screen
     setTimeout(function() {
-      // sakrij splash screen
       const splashScreens = document.querySelectorAll('.splash-screen');
       splashScreens.forEach(element => {
         element.style.display = 'none';
       });
   
-      // Preusmjeri  na home.html
       window.location.replace('home.html');
     }, 5000); // Vrijeme prikazivanja splash screena 
   });
